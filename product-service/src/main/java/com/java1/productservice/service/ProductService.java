993de919b750fc6e31cedd4d812dd46d8cda584d
Vinothini0806 <vinothinivijayakumaran08@gmail.com>
2023-07-24 16:33:30 +0530

    feat(product): Create the service for creating product

---
+++ product-service/src/main/java/com/java1/productservice/service/ProductService.java
@@ -0,0 +1,27 @@
+package com.java1.productservice.service;
+
+import com.java1.productservice.model.Product;
+import com.java1.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import com.java1.productservice.dto.ProductRequest;
+import com.java1.productservice.dto.ProductResponse;
+import java.util.List;
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+    private final ProductRepository productRepository;
+    public void createProduct(ProductRequest productRequest){
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+        productRepository.save(product);
+        log.info("Product {} is saved ",product.getId());
+    }
+
+    
+}
