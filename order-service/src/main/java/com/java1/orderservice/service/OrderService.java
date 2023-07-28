ab6a095fa148006a1995e240d2daa19e3677ae93
Vinothini0806 <vinothinivijayakumaran08@gmail.com>
2023-07-28 18:02:48 +0530

    feat(order): Create the service layer for to create the order

---
+++ order-service/src/main/java/com/java1/orderservice/service/OrderService.java
@@ -0,0 +1,38 @@
+package com.java1.orderservice.service;
+
+import com.java1.orderservice.dto.OrderLineItemsDto;
+import com.java1.orderservice.dto.OrderRequest;
+import com.java1.orderservice.model.Order;
+import com.java1.orderservice.model.OrderLineItems;
+import com.java1.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderRepository orderRepository;
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+        order.setOrderLineItemsList(orderLineItems);
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
+
+}
