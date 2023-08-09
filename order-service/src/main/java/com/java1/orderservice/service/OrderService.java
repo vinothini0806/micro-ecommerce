e2585dbbe8f701dc2b26c3c5b60d0b25f3e43c17
Vinothini0806 <vinothinivijayakumaran08@gmail.com>
2023-08-09 12:08:03 +0530

    feat(order): Add the functionality to check the available inventory

--- order-service/src/main/java/com/java1/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/java1/orderservice/service/OrderService.java
@@ -7,6 +7,7 @@ import com.java1.orderservice.model.OrderLineItems;
 import com.java1.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.UUID;
@@ -15,6 +16,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -26,7 +28,17 @@ public class OrderService {
         order.setOrderLineItemsList(orderLineItems);
         //call Inventroy Service, and place order if product is in
         //stock
-        orderRepository.save(order);
+        Boolean result = webClient.get()
+                .uri("http://localhost:8082/api/inventory")
+                .retrieve()
+                .bodyToMono(Boolean.class)
+                .block();
+        if(result){
+            orderRepository.save(order);
+        }else{
+            throw new IllegalArgumentException("Product is not in stock, Please try again later");
+        }
+
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
