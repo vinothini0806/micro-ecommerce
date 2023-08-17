cbb3fd906c74620a0e8a219456960fad27c836d6
Vinothini0806 <vinothinivijayakumaran08@gmail.com>
2023-08-17 20:52:31 +0530

    docs(discovery-server): Modify the order service

--- order-service/src/main/java/com/java1/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/java1/orderservice/service/OrderService.java
@@ -18,7 +18,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -34,7 +34,7 @@ public class OrderService {
                 .toList();
         //call InventoryService, and place order if product is in
         //stock
-        InventoryResponse[] inventoryResponseArray = webClient.get()
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
                 .uri("http://inventory-service/api/inventory",uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
