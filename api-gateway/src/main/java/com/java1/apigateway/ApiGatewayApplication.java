4763f142670e0f5f8986a35f6d7919fc2bb465a1
Vinothini0806 <vinothinivijayakumaran08@gmail.com>
2023-08-17 21:39:06 +0530

    feat(api-gateway): Implement api gateway application main function

--- api-gateway/src/main/java/com/java1/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/java1/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,12 @@
 package com.java1.apigateway;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ApiGatewayApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ApiGatewayApplication.class,args);
+    }
+
 }
